d8ccd3446706a1db232c4bd4fb840a7d8ea2dc5a
Sh3tss <gabrielggartist@gmail.com>
2025-05-13 20:12:49 -0300

    starting the rock paper and scissors game

--- rpsgame.js
+++ rpsgame.js
@@ -1,1 +1,6 @@
-console.log("Hellow Worlds!");
+function getComputerChoice(){
+    const choices =["rock","paper","scissors"];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+console.log(getComputerChoice);
