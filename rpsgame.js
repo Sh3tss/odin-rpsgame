91a6aed9b550e753660a4afec8cf8f0f09d92ad8
Sh3tss <gabrielggartist@gmail.com>
2025-05-15 20:51:34 -0300

    human choice part

--- rpsgame.js
+++ rpsgame.js
@@ -1,7 +1,23 @@
-function getComputerChoice(choices){
+function getComputerChoice(){
     const choices =["rock","paper","scissors"];
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
 const compChoice = getComputerChoice();
 console.log(compChoice);
+
+function getHumanChoice(){
+    const humanChoice = Number(window.prompt("Choose a number from 1, 2, 3"));
+    let aNumber = 0;
+    if (humanChoice === 1){
+        return aNumber = "rock";
+    }else if (humanChoice === 2){
+        return aNumber = "paper";
+    }else if (humanChoice === 3){
+        return aNumber = "scissors"
+    }
+}
+
+
+const huChoice = getHumanChoice();
+console.log(huChoice);
