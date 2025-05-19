0824766cdd2182a0ddce0b70fe7f775e7a852878
Sh3tss <gabrielggartist@gmail.com>
2025-05-19 18:38:44 -0300

    adding the 5 rounds

--- rpsgame.js
+++ rpsgame.js
@@ -20,27 +20,6 @@ function getHumanChoice(){
 }
 
 
-function playRound(huChoice, compChoice){
-    const huChoiceLower = huChoice.toLowerCase();
-    const compChoiceLower = compChoice.toLowerCase();
-
-    if (compChoiceLower === huChoiceLower){
-        console.log("It`s a tie. No one wins");
-    }else if (compChoiceLower === "paper" && huChoiceLower === "rock"){
-        computerScore++;
-        console.log(`You lose! ${compChoiceLower} beats ${huChoiceLower}`);
-    }else if (compChoiceLower === "scissors" && huChoiceLower === "paper"){
-        computerScore++;
-        console.log(`You lose! ${compChoiceLower} beats ${huChoiceLower}`);
-    }else if (compChoiceLower === "rock" && huChoiceLower === "scissors"){
-        computerScore++;
-        console.log(`You lose! ${compChoiceLower} beats ${huChoiceLower}`);
-    }else {
-        humanScore++;
-        console.log(`You win! ${huChoiceLower} beats ${compChoiceLower}`);
-    }
-}
-
 const huSelection = getHumanChoice();
 const compSelection = getComputerChoice();
 
@@ -48,9 +27,35 @@ playRound(huSelection, compSelection);
 console.log(humanScore, computerScore);
 
 function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
+    
+    for(let  i = 0; i <= 5; i++){
+        let humanScore = 0;
+        let computerScore = 0;
+
+        function playRound(huChoice, compChoice){
+        const huChoiceLower = huChoice.toLowerCase();
+        const compChoiceLower = compChoice.toLowerCase();
+
+        if (compChoiceLower === huChoiceLower){
+        console.log("It`s a tie. No one wins");
+        }else if (compChoiceLower === "paper" && huChoiceLower === "rock"){
+        computerScore++;
+        console.log(`You lose! ${compChoiceLower} beats ${huChoiceLower}`);
+        }else if (compChoiceLower === "scissors" && huChoiceLower === "paper"){
+        computerScore++;
+        console.log(`You lose! ${compChoiceLower} beats ${huChoiceLower}`);
+        }else if (compChoiceLower === "rock" && huChoiceLower === "scissors"){
+        computerScore++;
+        console.log(`You lose! ${compChoiceLower} beats ${huChoiceLower}`);
+        }else {
+        humanScore++;
+        console.log(`You win! ${huChoiceLower} beats ${compChoiceLower}`);
+        }
+    }
+    }
+    
 }
+
 
 
 
